30840aca7f4f9684aa262e2ee011a97eddf37d97
litongjava <litongjava@qq.com>
2023-10-13 02:31:17 -1000

    制定输出的目录

--- build.py
+++ build.py
@@ -6,17 +6,22 @@ def construct_pyinstaller_command():
     # Get the system name (e.g., 'Windows', 'Linux', 'Darwin' for macOS, etc.)
     system_name = platform.system()
 
-    # Get the architecture
-    arch_name = platform.architecture()[0]
+    # Get the detailed architecture identifier
+    arch_name = platform.machine()
 
+    output_name="pandora-cloud"
     # Construct the output name
-    output_name = f"pandora-cloud-{system_name.lower()}-{arch_name}"
+    output_folder_name = f"{output_name}-{system_name.lower()}-{arch_name}"
 
     # Determine the appropriate path separator for --add-data based on the OS
     path_separator = ";" if system_name == "Windows" else ":"
 
+    # Specify the output directory for the bundled app
+    dist_path = f"dist/{output_folder_name}"
+
     # Construct the PyInstaller command
     command = (f'pyinstaller --onefile --name {output_name} '
+               f'--distpath {dist_path} '
                f'--add-data "src/pandora_cloud/flask/static{path_separator}flask/static" '
                f'--add-data "src/pandora_cloud/flask/templates{path_separator}flask/templates" main.py')
 
